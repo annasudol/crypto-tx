5bbe2707bc25cb21fe0bf65e318bd64912d9cb1d
Anna Sudol <anjasudol@gmail.com>
2022-12-27 11:31:57 +0000

    cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "cypress";
 import cypressMetamask from '@positionex/cypress-metamask-v3/cypress/plugins';
-// Load local .env
-// SECRET_WORDS = "that olive candy yellow void certain burden result spike leader dose news"
-// PASSWORD = TestMetaMask
-// METAMASK_VERSION = latest
-// NETWORK_NAME = PolygonMumbai
-// RPC_URL = https://matic-mumbai.chainstacklabs.com
-//   =80001
+
 require('dotenv').config();
 
 export default defineConfig({
